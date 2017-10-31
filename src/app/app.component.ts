6e1566695dbc91bc02145340047ac3d15b2a4e35
Jorge Vergara <javebratt@gmail.com>
2017-10-31 14:55:41 -0500

    Bulk Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,30 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    afAuth: AngularFireAuth
+  ) {
+    const authListener = afAuth.authState.subscribe(user => {
+      if (user) {
+        this.rootPage = HomePage;
+        authListener.unsubscribe();
+      } else {
+        this.rootPage = 'LandingPage';
+        authListener.unsubscribe();
+      }
+    });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,4 +34,3 @@ export class MyApp {
     });
   }
 }
-
---
+++ src/pages/bill-detail/bill-detail.ts
@@ -0,0 +1,121 @@
+import { Component } from '@angular/core';
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  ActionSheet,
+  ActionSheetController,
+  Platform,
+  Alert,
+  AlertController
+} from 'ionic-angular';
+import { BillProvider } from '../../providers/bill/bill';
+import { Observable } from 'rxjs/Observable';
+import { AuthProvider } from '../../providers/auth/auth';
+import { Camera } from '@ionic-native/camera';
+
+@IonicPage({
+  segment: 'bill/:billId'
+})
+@Component({
+  selector: 'page-bill-detail',
+  templateUrl: 'bill-detail.html'
+})
+export class BillDetailPage {
+  public bill: {};
+  public billId: string;
+  public placeholderPicture: string = 'assets/img/debt-collector.jpg';
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public actionCtrl: ActionSheetController,
+    public platform: Platform,
+    public alertCtrl: AlertController,
+    public billProvider: BillProvider,
+    public authProvider: AuthProvider,
+    public cameraPlugin: Camera
+  ) {}
+
+  ionViewDidEnter() {
+    this.billId = this.navParams.get('billId');
+    this.billProvider
+      .getBill(this.billId)
+      .valueChanges()
+      .subscribe(bill => {
+        this.bill = bill;
+      });
+  }
+
+  showOptions(billId): void {
+    const action: ActionSheet = this.actionCtrl.create({
+      title: 'Modify your bill',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: !this.platform.is('ios') ? 'trash' : null,
+          handler: () => {
+            this.billProvider.removeBill(billId).then(() => {
+              this.navCtrl.pop();
+            });
+          }
+        },
+        {
+          text: 'Mark as Paid!',
+          icon: !this.platform.is('ios') ? 'checkmark' : null,
+          handler: () => {
+            this.billProvider.payBill(billId);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          icon: !this.platform.is('ios') ? 'close' : null,
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    action.present();
+  }
+
+  uploadPicture(): void {
+    if (this.authProvider.getUser().isAnonymous == true) {
+      const alert: Alert = this.alertCtrl.create({
+        message:
+          'If you want to continue you will need to provide an email and create a password',
+        buttons: [
+          { text: 'Cancel' },
+          {
+            text: 'OK',
+            handler: data => {
+              this.navCtrl.push('SignupPage');
+            }
+          }
+        ]
+      });
+      alert.present();
+    } else {
+      this.cameraPlugin
+        .getPicture({
+          quality: 95,
+          destinationType: this.cameraPlugin.DestinationType.DATA_URL,
+          sourceType: this.cameraPlugin.PictureSourceType.CAMERA,
+          allowEdit: true,
+          encodingType: this.cameraPlugin.EncodingType.PNG,
+          targetWidth: 500,
+          targetHeight: 500,
+          saveToPhotoAlbum: true
+        })
+        .then(
+          imageData => {
+            this.billProvider.takeBillPhoto(this.billId, imageData);
+          },
+          error => {
+            console.log('ERROR -> ' + JSON.stringify(error));
+          }
+        );
+    }
+  }
+}
