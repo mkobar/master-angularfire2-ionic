906d085889443617e562ed7e1afd015b55da3ce9
Jorge Vergara <javebratt@gmail.com>
2017-12-02 09:36:36 -0500

    Start upgrading the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,30 +2,15 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { AngularFireAuth } from 'angularfire2/auth';
 
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage: any;
+  rootPage:any = HomePage;
 
-  constructor(
-    platform: Platform,
-    statusBar: StatusBar,
-    splashScreen: SplashScreen,
-    afAuth: AngularFireAuth
-  ) {
-    const authListener = afAuth.authState.subscribe(user => {
-      if (user) {
-        this.rootPage = HomePage;
-        authListener.unsubscribe();
-      } else {
-        this.rootPage = 'LandingPage';
-        authListener.unsubscribe();
-      }
-    });
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -34,3 +19,4 @@ export class MyApp {
     });
   }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,46 +6,25 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { AuthProvider } from '../providers/auth/auth';
-import { BillProvider } from '../providers/bill/bill';
-import { Camera } from '@ionic-native/camera';
-
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { firebaseConfig } from './credentials';
-
-class CameraMock extends Camera {
-  getPicture(options) {
-    return new Promise((resolve, reject) => {
-      resolve(`TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIG
-        J1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGl
-        jaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2Yg
-        ZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb
-        24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IG
-        Nhcm5hbCBwbGVhc3VyZS4=`);
-    });
-  }
-}
 
 @NgModule({
-  declarations: [MyApp, HomePage],
+  declarations: [
+    MyApp,
+    HomePage
+  ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
-    AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireAuthModule,
-    AngularFireDatabaseModule
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
-  entryComponents: [MyApp, HomePage],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: Camera, useClass: CameraMock },
-    { provide: ErrorHandler, useClass: IonicErrorHandler },
-    AuthProvider,
-    BillProvider
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
