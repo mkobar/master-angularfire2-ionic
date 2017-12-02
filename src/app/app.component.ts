b28fa67b8abde9fe6d61846688a2b341d0aaf1ac
Jorge Vergara <javebratt@gmail.com>
2017-12-02 10:09:04 -0500

    Adds landing page update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,30 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { AngularFireAuth } from 'angularfire2/auth';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    afAuth: AngularFireAuth
+  ) {
+    const authListener = afAuth.authState.subscribe(user => {
+      if (user) {
+        this.rootPage = HomePage;
+        authListener.unsubscribe();
+      } else {
+        this.rootPage = 'LandingPage';
+        authListener.unsubscribe();
+      }
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,4 +36,3 @@ export class MyApp {
     });
   }
 }
-
