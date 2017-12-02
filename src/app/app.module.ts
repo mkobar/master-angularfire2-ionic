77cd03f2c0efe658b6046191fe4c0a39848a2320
Jorge Vergara <javebratt@gmail.com>
2017-12-02 09:43:59 -0500

    Adds Mock class for the Camera plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,19 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { firebaseConfig } from './credentials';
 
+class CameraMock extends Camera {
+  getPicture(options) {
+    return new Promise((resolve, reject) => {
+      resolve(`TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIG
+        J1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGl
+        jaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2Yg
+        ZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb
+        24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IG
+        Nhcm5hbCBwbGVhc3VyZS4=`);
+    });
+  }
+}
+
 @NgModule({
   declarations: [MyApp, HomePage],
   imports: [
@@ -29,7 +42,7 @@ import { firebaseConfig } from './credentials';
   providers: [
     StatusBar,
     SplashScreen,
-    Camera,
+    { provide: Camera, useClass: CameraMock },
     { provide: ErrorHandler, useClass: IonicErrorHandler },
     AuthProvider,
     BillProvider
