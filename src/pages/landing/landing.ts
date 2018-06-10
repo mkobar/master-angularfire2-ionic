8c0badce2cb481b8be9584835de3a01880641ace
Jorge Vergara <javebratt@gmail.com>
2018-06-10 12:13:56 -0500

    Adds the landing page

--- src/pages/landing/landing.ts
+++ src/pages/landing/landing.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the LandingPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {
+  IonicPage,
+  NavController,
+  Loading,
+  LoadingController
+} from 'ionic-angular';
+import { HomePage } from '../home/home';
+import { AuthProvider } from '../../providers/auth/auth';
 
 @IonicPage()
 @Component({
@@ -14,12 +14,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'landing.html',
 })
 export class LandingPage {
+  constructor(
+    public navCtrl: NavController,
+    public loadingCtrl: LoadingController,
+    public authProvider: AuthProvider
+  ) { }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  goToLogin(): void { this.navCtrl.push('LoginPage'); }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LandingPage');
+  goToBillList(): void {
+    this.authProvider.anonymousLogin().then(newUser => {
+      loading.dismiss().then(() => {
+        this.navCtrl.setRoot(HomePage);
+      });
+    }).catch(error => {
+      loading.dismiss().then(() => {
+        console.error(error);
+      });
+    });
+
+    const loading: Loading = this.loadingCtrl.create();
+    loading.present();
   }
 
 }
