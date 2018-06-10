04e9a32d88c0614b83a1f5a7dbf6daa54752dfe8
Jorge Vergara <javebratt@gmail.com>
2018-06-10 12:50:28 -0500

    Adds the CreateBill page

--- src/pages/bill-create/bill-create.ts
+++ src/pages/bill-create/bill-create.ts
@@ -1,25 +1,46 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the BillCreatePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BillProvider } from '../../providers/bill/bill';
 
 @IonicPage()
 @Component({
   selector: 'page-bill-create',
-  templateUrl: 'bill-create.html',
+  templateUrl: 'bill-create.html'
 })
 export class BillCreatePage {
+  public newBillForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    formBuilder: FormBuilder,
+    public billProvider: BillProvider
+  ) {
+    this.newBillForm = formBuilder.group({
+      name: ['', Validators.required],
+      amount: ['', Validators.required],
+      dueDate: ['', Validators.required]
+    });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad BillCreatePage');
+  createBill() {
+    if (!this.newBillForm.valid) {
+      console.log(this.newBillForm.value);
+    } else {
+      this.billProvider
+        .createBill(
+          this.newBillForm.value.name,
+          this.newBillForm.value.amount,
+          this.newBillForm.value.dueDate
+        )
+        .then(
+          () => {
+            this.navCtrl.pop();
+          },
+          error => {
+            console.log(error);
+          }
+        );
+    }
   }
-
 }
