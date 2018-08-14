1429a50caa4dd16938cb9d5b8dd5e0d02d4165aa
mkobar <mike@exlorev.com>
2018-08-14 10:52:18 -0400

    new creds

--- src/app/credentials.ts
+++ src/app/credentials.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBJTmt3LGBm5CTiO5DUZ4W3O5mTLphaoKY",
-  authDomain: "javebratt-playground.firebaseapp.com",
-  databaseURL: "https://javebratt-playground.firebaseio.com",
-  projectId: "javebratt-playground",
-  storageBucket: "javebratt-playground.appspot.com",
-  messagingSenderId: "369908572440"
+  apiKey: 'AIzaSyCSnWtftN2EoDmgZ6uPpWavAAlNKp0CXWM',
+  authDomain: 'ionic-fb-javebratt.firebaseapp.com',
+  databaseURL: 'https://ionic-fb-javebratt.firebaseio.com',
+  projectId: 'ionic-fb-javebratt',
+  storageBucket: '',
+  messagingSenderId: '61384504779',
 };
